4dd21106be97853b95fb12e274773deb4861785d
Raymond Chen <ahrchen@gmail.com>
2023-06-26 20:35:25 -0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Raymond Chen on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
